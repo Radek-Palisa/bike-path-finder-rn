2195911872cf6595270e5fe779a76e1fd27bf653
Radek Palisa <radek.palisa@gmail.com>
2021-12-20 15:57:36 +0100

    add eslint and prettier

--- App.tsx
+++ App.tsx
@@ -4,30 +4,33 @@ import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
+      const location = await Location.getCurrentPositionAsync({});
 
       setLocation(location);
     })();
   }, []);
-  
+
   return (
     <View style={styles.container}>
-      <MapView 
-        provider={PROVIDER_GOOGLE} 
+      <MapView
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         showsUserLocation
-        showsMyLocationButton />
+        showsMyLocationButton
+      />
     </View>
   );
 }
@@ -42,5 +45,5 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-  }
+  },
 });
