3a16d6817eda01a4f7d429ca48dc1f01661ea34a
Radek Palisa <radek.palisa@gmail.com>
2021-12-26 18:18:52 +0100

    station capacity visual representation

--- src/scenes/Map/components/StationMarkers.tsx
+++ src/scenes/Map/components/StationMarkers.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import { Marker } from 'react-native-maps';
 import { StationsInfo } from '../services/types';
 import BikeStationIcon from '../../../components/icons/BikeStationIcon';
+import getStationAvailability from '../services/getStationAvailability/getStationAvailability';
 
 type Props = {
   data: StationsInfo;
@@ -11,15 +12,35 @@ type Props = {
 export default memo(function StationMarkers({ data }: Props) {
   return (
     <>
-      {[...data].map(([stationId, { coordinate, shouldShowStatus }]) => (
-        <Marker key={stationId} coordinate={coordinate}>
-          {shouldShowStatus ? (
-            <BikeStationIcon fillPercentage={0.5} />
-          ) : (
-            <View style={styles.container} />
-          )}
-        </Marker>
-      ))}
+      {[...data].map(
+        ([
+          stationId,
+          {
+            coordinate,
+            shouldShowStatus,
+            capacity,
+            availableMechanical,
+            availableElectric,
+          },
+        ]) => {
+          const availableTotal = availableMechanical + availableElectric;
+
+          return (
+            <Marker key={stationId} coordinate={coordinate}>
+              {shouldShowStatus ? (
+                <BikeStationIcon
+                  fillPercentage={getStationAvailability(
+                    availableTotal,
+                    capacity
+                  )}
+                />
+              ) : (
+                <View style={styles.container} />
+              )}
+            </Marker>
+          );
+        }
+      )}
     </>
   );
 });
