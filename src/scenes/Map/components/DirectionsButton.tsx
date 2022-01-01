d834c90b09382e478fb2c2a24b11bf14cd455000
Radek Palisa <radek.palisa@gmail.com>
2022-01-01 17:26:37 +0100

    DirectionsIcon

--- src/scenes/Map/components/DirectionsButton.tsx
+++ src/scenes/Map/components/DirectionsButton.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import DirectionsIcon from '../../../components/icons/DirectionsIcon';
 
 type Props = {
   onPress: () => void;
@@ -7,6 +8,7 @@ type Props = {
 export default function DirectionsButton({ onPress }: Props) {
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
+      <DirectionsIcon />
       <Text style={styles.text}>Directions</Text>
     </TouchableOpacity>
   );
@@ -16,15 +18,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 0,
     flexDirection: 'row',
+    alignSelf: 'flex-start',
     justifyContent: 'center',
     alignItems: 'center',
     height: 40,
     paddingHorizontal: 10,
+    paddingLeft: 15,
     backgroundColor: '#4181EC',
     borderRadius: 36,
-    // fontWeight: '500', // doesn't work
+    fontWeight: 'bold', // doesn't work
   },
   text: {
     color: '#fff',
+    paddingHorizontal: 10,
   },
 });
