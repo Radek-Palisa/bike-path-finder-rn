7395e4ef994a1654279bf68e7b358ad9b704b0fd
Radek Palisa <radek.palisa@gmail.com>
2022-01-06 14:11:38 +0100

    update DirectionsPanelMenu info

--- src/scenes/Map/components/DirectionsPanelMenu.tsx
+++ src/scenes/Map/components/DirectionsPanelMenu.tsx
@@ -1,4 +1,6 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import DirectionsBikeIcon from '../../../components/icons/DirectionsBikeIcon';
+import DirectionsWalkIcon from '../../../components/icons/DirectionsWalkIcon';
 import { Directions } from '../services/types';
 import CancelDirectionsButton from './CancelDirectionsButton';
 
@@ -15,12 +17,64 @@ export default function DirectionsPanelMenu({
     <View>
       <CancelDirectionsButton onPress={onDirectionsClearPress} />
       {directionsData && (
-        <Text>
-          {directionsData.walkingToStation.duration?.text} -{' '}
-          {directionsData.cycling[0].duration?.text} -{' '}
-          {directionsData.walkingToDestination.duration?.text}
-        </Text>
+        <>
+          <View style={styles.directionsInfo}>
+            <Text style={styles.duration}>
+              {directionsData.cycling[0].totalDuration.text}
+            </Text>
+            <Text style={styles.distance}>
+              ({directionsData.cycling[0].totalDistance.text})
+            </Text>
+          </View>
+          <View style={styles.durationBreakdown}>
+            <DirectionsWalkIcon width={20} />
+            <Text style={styles.durationBreakdownText}>
+              {directionsData.walkingToStation.duration?.text}
+            </Text>
+            <DirectionsBikeIcon
+              width={20}
+              style={styles.durationBreakdownBikeIcon}
+            />
+            <Text style={styles.durationBreakdownText}>
+              {directionsData.cycling[0].duration?.text}
+            </Text>
+            <DirectionsWalkIcon width={20} />
+            <Text style={styles.durationBreakdownText}>
+              {directionsData.walkingToDestination.duration?.text}
+            </Text>
+          </View>
+        </>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  directionsInfo: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginTop: 10,
+    paddingLeft: 10,
+  },
+  duration: {
+    fontSize: 20,
+  },
+  distance: {
+    fontSize: 20,
+    color: 'gray',
+    paddingHorizontal: 10,
+  },
+  durationBreakdown: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginTop: 10,
+    paddingLeft: 10,
+  },
+  durationBreakdownText: {
+    color: 'gray',
+    paddingLeft: 2,
+  },
+  durationBreakdownBikeIcon: {
+    marginHorizontal: 4,
+  },
+});
