39fffb6e8644068e247516e68549118a6ba092ed
Radek Palisa <radek.palisa@gmail.com>
2022-01-30 22:36:34 +0100

    animate RefreshDataButton

--- src/scenes/Map/components/RefreshDataButton.tsx
+++ src/scenes/Map/components/RefreshDataButton.tsx
@@ -1,6 +1,12 @@
 import { useRef } from 'react';
-import { StyleSheet, View } from 'react-native';
-import FloatButton from '../../../components/FloatButton';
+import { Pressable, StyleSheet, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withDelay,
+  Easing,
+} from 'react-native-reanimated';
 import LoopIcon from '../../../components/icons/LoopIcon';
 
 const REFRESH_DATA_THROTTLE_MS = 4000;
@@ -11,8 +17,22 @@ type Props = {
 
 export default function RefreshDataButton({ onPress }: Props) {
   const isThrottling = useRef<boolean>(false);
+  const offset = useSharedValue(0);
+  const opacity = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ rotateZ: `${offset.value}deg` }],
+  }));
+  const animatedOpacity = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
 
   const handlePress = () => {
+    offset.value = withTiming(offset.value - 360, {
+      duration: 1000,
+      easing: Easing.bezier(0.25, 1, 0.5, 1),
+    });
+    opacity.value += 1;
+    opacity.value = withDelay(900, withTiming(0, { duration: 300 }));
     if (isThrottling.current) return;
     isThrottling.current = true;
     onPress();
@@ -21,19 +41,49 @@ export default function RefreshDataButton({ onPress }: Props) {
 
   return (
     <View style={styles.container}>
-      <FloatButton
-        onPress={handlePress}
+      <Pressable
+        android_disableSound
         accessibilityLabel="refetch bike stations information"
-        icon={<LoopIcon />}
-      />
+        onPress={handlePress}
+      >
+        <View style={styles.iconContainer}>
+          <LoopIcon />
+        </View>
+        <Animated.View
+          style={[
+            styles.iconContainer,
+            styles.animatedIconContainer,
+            animatedStyles,
+            animatedOpacity,
+          ]}
+        >
+          <LoopIcon fill="#4181EC" />
+        </Animated.View>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: 42,
+    height: 42,
+    borderRadius: 42,
+    backgroundColor: '#fff',
+    elevation: 6,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+  },
+  iconContainer: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animatedIconContainer: {
     position: 'absolute',
-    top: 40,
-    right: 20,
+    backgroundColor: 'white',
+    borderRadius: 42,
   },
 });
