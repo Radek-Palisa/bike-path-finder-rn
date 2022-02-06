361b579e6ee24bbce4daa94ea15cedb4ae76f01f
Radek Palisa <radek.palisa@gmail.com>
2022-02-06 12:14:08 +0200

    install Firebase Web SDK

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import Constants from 'expo-constants';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Map from './src/scenes/Map/Map';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppLoading from 'expo-app-loading';
 import SignIn from './src/scenes/SignIn/SignIn';
+
+const configKeys = Constants.manifest!.extra!;
+
+const firebaseConfig = {
+  apiKey: configKeys.firebaseApiKey,
+  authDomain: configKeys.firebaseAuthDomain,
+  projectId: configKeys.firebaseProjectId,
+  storageBucket: configKeys.firebaseStorageBucket,
+  messagingSenderId: configKeys.firebaseMessagingSenderId,
+  appId: configKeys.firebaseAppId,
+  measurementId: configKeys.firebaseMeasurementId,
+};
+
+initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 
