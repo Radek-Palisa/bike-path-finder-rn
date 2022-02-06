00dfcd52aee38677ba5a36dc551c9b8709b9fc74
Radek Palisa <radek.palisa@gmail.com>
2022-02-06 11:09:31 +0200

    add AppLoading & SignIn screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Map from './src/scenes/Map/Map';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AppLoading from 'expo-app-loading';
+import SignIn from './src/scenes/SignIn/SignIn';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const isSignedIn = true;
+
+  if (isSignedIn === null) {
+    return <AppLoading />;
+  }
+
   return (
     <SafeAreaProvider>
-      <Map />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Home"
+              component={Map}
+            />
+          ) : (
+            <Stack.Screen name="Sign In" component={SignIn} />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
